5b9450a8b7a70c6732bfb87dc228e1074573071e
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-05-09 00:03:22 +0200

    async gallery

--- forntend/src/app/(navPages)/gallery/page.jsx
+++ forntend/src/app/(navPages)/gallery/page.jsx
@@ -1,21 +1,20 @@
 // pages/Gallery.js
 import Head from 'next/head';
 import InstagramFeed from "@/components/instagram/InstagramFeed";
-import InstagramEmbed from '@/components/instagram/StaticInstagram';
+
 
 export default function Gallery() {
   return (
     <>
-      <Head>
+     <Head>
         <title>Gallery | Our Site</title>
         <meta name="description" content="Explore our curated gallery featuring Instagram content." />
-        {/* <script src="https://cdn.lightwidget.com/widgets/lightwidget.js" async></script> */}
-        {/* <script src="https://cdn.lightwidget.com/widgets/lightwidget.js" ></script> */}
+        {/* The async attribute is already set, but ensure that no "defer" or other attributes interfere */}
         <script src="https://cdn.lightwidget.com/widgets/lightwidget.js" async></script>
       </Head>
       <main className="min-h-screen bg-gray-200 p-5">
         <h1 className="text-center text-4xl font-bold my-8 text-gray-800">Welcome to Our Gallery</h1>
-        <section >
+        <section>
           <InstagramFeed />
         </section>
       </main>
