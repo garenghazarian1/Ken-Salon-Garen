1cf1f38d7f6610f9159e4528cb432eeee0c8ce69
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-05-18 01:12:08 +0200

    styling services

--- forntend/src/app/stores/[storeId]/salon-services/page.jsx
+++ forntend/src/app/stores/[storeId]/salon-services/page.jsx
@@ -58,6 +58,7 @@ const handleNavigation = () => {
 };
 return (
   <div className={styles.container}>
+    {/* <p >swipe to left &#9756;</p> */}
     <h1 className={styles.title}>{activeSection}</h1>
     
     <div className={styles.section}>
@@ -65,10 +66,12 @@ return (
         <div key={category} className={styles.section}>
           <h2 className={styles.sectionTitle}>{category}</h2>
           <Swiper spaceBetween={10} slidesPerView={5}  pagination={{ clickable: true }} breakpoints={{
-                  320: { slidesPerView: 1, spaceBetween: 10 },
-                  660: { slidesPerView: 2, spaceBetween: 10 },
-                  1024: { slidesPerView: 3, spaceBetween: 10 },
-                  1440: { slidesPerView: 4, spaceBetween: 10 }
+                 320: { slidesPerView: 1, spaceBetween: 10 },
+                  480: { slidesPerView: 1, spaceBetween: 10 },
+                  520: { slidesPerView: 2, spaceBetween: 10 },
+                  800: { slidesPerView: 3, spaceBetween: 10 },
+                  1024: { slidesPerView: 4, spaceBetween: 10 },
+                  1440: { slidesPerView: 5, spaceBetween: 10 }
                 }}
                 >
             {services.map((service) => (
