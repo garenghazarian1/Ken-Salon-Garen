ee255f94d21bd5355ed1a85d7df27496696eb8f5
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-06-22 21:53:47 +0200

    corrections

--- forntend/src/components/navbar/HamburgerButton/HamburgerButton.jsx
+++ forntend/src/components/navbar/HamburgerButton/HamburgerButton.jsx
@@ -5,7 +5,7 @@ const HamburgerButton = ({ isOpen, toggle }) => {
     return (
       <button
         onClick={toggle}
-        className="hamburger-btn lg:hidden fixed top-4 right-8 z-50 flex flex-col justify-center items-center w-12 h-12 rounded-md bg-transparent hover:bg-teal-900 "
+        className="hamburger-btn lg:hidden fixed top-4 right-8 z-50 flex flex-col justify-center items-center w-12 h-12 rounded-md bg-transparent  border-2 border-transparent hover:border-green-200 "
         aria-label="Toggle menu"
       >
         <div className={`w-8 h-1 bg-green-100 mb-2 transition-transform duration-300 ease-in-out ${isOpen ? 'rotate-45 translate-y-3.5' : ''}`}></div>
