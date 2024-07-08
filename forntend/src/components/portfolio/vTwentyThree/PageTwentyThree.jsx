6b82f4dad01b91773946a360e75f339baf665644
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-07-08 12:40:20 +0200

    portfolio page twentythree

--- forntend/src/components/portfolio/vTwentyThree/PageTwentyThree.jsx
+++ forntend/src/components/portfolio/vTwentyThree/PageTwentyThree.jsx
@@ -3,29 +3,16 @@ import styles from "./PageTwentyThree.module.css"
 export default function PageTwentyThree() {
   return (
     <>
-    <div>
-      <div>
-        <p>We&apos;ve etched our name as the pinnacle of service and artistry in the hair and beauty domain, where every moment is infused with passion and dedication. Our unyielding commitment to excellence has forged bonds of trust with cherished celebrities, who turn to us for their most intimate beauty journeys. It fills us with profound pride to have walked alongside luminaries from diverse backgrounds, sharing in their dreams and aspirations.</p>
+    <div className={styles.container}>
+      <div className={styles.sectionB}>
+        <p className={styles.text}>We&apos;ve etched our name as the pinnacle of service and artistry in the hair and beauty domain, where every moment is infused with passion and dedication. Our unyielding commitment to excellence has forged bonds of trust with cherished celebrities, who turn to us for their most intimate beauty journeys. It fills us with profound pride to have walked alongside luminaries from diverse backgrounds, sharing in their dreams and aspirations.</p>
       </div>
-      
-      
-      
-      
-      
-      
-      
-      
+
       <div>
-        <div>
-          <p>Lamis Khan</p>
-          <p>Mahira Abdel Aziz</p>
-          <p>Loujain Adada</p>
-          <p>Model Roz</p>
-          <p>Alaa Bu Harb</p>
-          <p>Farah Hassan</p>
-          <p>Carmen Soliman</p>
-          <p>Ameni Esseibii</p>
-        </div>
+       
+         <div className={styles.sectionA}>
+                <video src="/portfolio/Celeb01.mp4" alt="Salon Video" autoPlay muted loop playsInline preload="auto" className={styles.video}/>
+            </div>
 
 
       </div>
