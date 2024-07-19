06d50bb59525479b83c489eab761b96d21f76ea0
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-07-19 23:45:40 +0200

    edit loading

--- forntend/src/components/loading/LoadingSkeleton.jsx
+++ forntend/src/components/loading/LoadingSkeleton.jsx
@@ -4,8 +4,7 @@ import styles from './LoadingSkeleton.module.css';
 export default function LoadingSkeleton() {
   return (
     <div className={styles.container}>
-      
-      <Image src="/logo02.png" alt="loading" loading="lazy" as="image" width={100} height={100}   className={styles.logo} />
+      <Image src="/logo03.png" alt="loading" loading="lazy" as="image" width={100} height={100}   className={styles.logo} />
     </div>
   );
 }
