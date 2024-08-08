d35f06664d3307a0c6b0c76e752f4cbd3fac9029
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-08-08 11:32:24 +0200

    chatbot

--- forntend/src/app/layout.jsx
+++ forntend/src/app/layout.jsx
@@ -1,22 +1,21 @@
-
 // import { Inter, Lora} from "next/font/google";
-import {lora, inter} from "@/app/ui/fonts"
+import { lora, inter } from "@/app/ui/fonts";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import { UserProvider } from "../context/UserContext.jsx";
 import AuthProvider from "@/context/auth";
-import { CalendarProvider } from '@/context/CalendarContext';
-import { StoreProvider } from '@/context/StoreContext';
-import { ServiceProvider } from '@/context/ServiceContext';
+import { CalendarProvider } from "@/context/CalendarContext";
+import { StoreProvider } from "@/context/StoreContext";
+import { ServiceProvider } from "@/context/ServiceContext";
 import { EmployeeProvider } from "@/context/EmployeeContext";
 import { DateProvider } from "@/context/DateContext";
 import { AppointmentProvider } from "@/context/AppointmentContext";
-import styles from './Layout.module.css';
+import styles from "./Layout.module.css";
 
 // const inter = Inter({ subsets: ["latin"] });
 // const arapeyStatic = Lora ({
-//   weight: '400',  
+//   weight: '400',
 //   display: 'swap',
 //   subsets: ['latin']
 // });
@@ -29,33 +28,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.className}, ${lora.className} `}>
-    
-      <body >
+      <body>
         <div className={styles.layoutContainer}>
           <AuthProvider>
-              <UserProvider>
-               <StoreProvider>
+            <UserProvider>
+              <StoreProvider>
                 <ServiceProvider>
-                 <CalendarProvider> 
-                  <EmployeeProvider>
-                  <DateProvider>
-                  <AppointmentProvider>
-                    <Navbar />
-                    
-                      <div className={styles.childrenContainer}>
-                        {children}
-                      </div>
-                     
-                    <Footer />
-                    </AppointmentProvider>
-                    </DateProvider>
-                  </EmployeeProvider>  
-                </CalendarProvider> 
+                  <CalendarProvider>
+                    <EmployeeProvider>
+                      <DateProvider>
+                        <AppointmentProvider>
+                          <Navbar />
+
+                          <div className={styles.childrenContainer}>
+                            {children}
+                          </div>
+
+                          <Footer />
+                        </AppointmentProvider>
+                      </DateProvider>
+                    </EmployeeProvider>
+                  </CalendarProvider>
                 </ServiceProvider>
-                </StoreProvider>
+              </StoreProvider>
             </UserProvider>
           </AuthProvider>
         </div>
+        <script
+          src="//code.tidio.co/wuxjaut5j0iru5lnq6efealcjpqmlyhw.js"
+          async
+        ></script>
       </body>
     </html>
   );
